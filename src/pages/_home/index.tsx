0516e7a2bde20f662cf2edca83dc23beeb778ada
Rychell Sousa <rychell@gmail.com>
2021-04-27 21:12:02 -0300

    moved style file to a proper folder

--- src/pages/_home/index.tsx
+++ src/pages/_home/index.tsx
@@ -5,7 +5,7 @@ import api from '../../services/api';
 import Food from '../../components/Food';
 import ModalAddFood from '../../components/ModalAddFood';
 import ModalEditFood from '../../components/ModalEditFood';
-import { FoodsContainer } from './styles';
+import { FoodsContainer } from '../../styles/styles';
 
 interface DashboardProps {
   products: IFood[],
